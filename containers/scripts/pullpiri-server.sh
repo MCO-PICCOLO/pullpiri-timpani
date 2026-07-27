86898bb594f1997919a84840d3cd79eee03d74f0
Youngtae Kim <youngtae88.kim@lge.com>
2026-07-27 13:48:45 +0900

    Change PullPiri server version to v0.7.2-dev.2
    Updated the VERSION variable to 'v0.7.2-dev.2' for the PullPiri server.

--- containers/scripts/pullpiri-server.sh
+++ containers/scripts/pullpiri-server.sh
@@ -12,7 +12,8 @@ fi
 ROCKSDB_VERSION="v11.18.0"
 ROCKSDB_IMAGE="ghcr.io/mco-piccolo/pullpiri-rocksdb:${ROCKSDB_VERSION}"
 
-VERSION="latest"
+# VERSION="latest"
+VERSION="v0.7.2-dev.2"
 CONTAINER_IMAGE="ghcr.io/eclipse-pullpiri/pullpiri:${VERSION}"
 # If you want to use a locally built image, uncomment the line below and comment out the line above
 # CONTAINER_IMAGE="localhost/pullpiri:latest"
